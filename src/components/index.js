a8fce2075f9b7a8b13638e4d0def3c19278b1d80
Sainath <svmsainath@ymail.com>
2022-03-13 17:02:08 +0530

    Added Indentation

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import { Header } from "./header/Header";
 import { Footer } from "./footer/Footer";
-import {Main} from "./main/Main"
+import { Main } from "./main/Main"
 
-export {Header, Footer, Main} ;
+export { Header, Footer, Main };
